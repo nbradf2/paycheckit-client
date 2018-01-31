b9d92b99488245b1815317afed30f59a9c9d2a30
Nicole Bradford <nbradf2@gmail.com>
2018-01-30 21:06:40 -0600

    add app.js and test

---
+++ src/components/app.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import App from './app';
+
+describe('<App />', () => {
+	it('Renders without crashing', () => {
+		shallow(<App />);
+	})
+})
