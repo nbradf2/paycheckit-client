bceaecbd1d6f75a09cb3e3ac61ecd29357df6cd3
Nicole Bradford <nbradf2@gmail.com>
2018-01-30 21:18:46 -0600

    fix import of Dashboard

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './components/app'
+import {Dashboard} from './components/dashboard';
 import './index.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
 
