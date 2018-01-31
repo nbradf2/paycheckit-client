e72357742516686e2d091e82a5979be823fe4903
Nicole Bradford <nbradf2@gmail.com>
2018-01-30 21:21:25 -0600

    remove {} from Dashboard

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Dashboard} from './components/dashboard';
+import Dashboard from './components/dashboard';
 import './index.css';
 
 
