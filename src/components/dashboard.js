02874905115b7d3ffd1a5bad73af961973fca725
Nicole Bradford <nbradf2@gmail.com>
2018-01-31 21:14:26 -0600

    update about styling

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -9,17 +9,24 @@ export default function Dashboard(props) {
 		<div>
 			{/** NAV BAR **/}
 			<section>
-				<h1>Paycheck<span>It</span></h1>
-				<p>Because living paycheck to paycheck doesn't have to be so hard.</p>
-				<i className="fas fa-3x fa-angle-double-down"></i>
+				<div className="title">
+					<h1>Paycheck<span>It</span></h1>
+					<p>Because living paycheck to paycheck doesn't have to be so hard.</p>
+				</div>
+				<i className="fas fa-3x fa-angle-double-down" data-fa-transform="down-60"></i>
 			</section>
 			<section>
-				<i className="fas fa-tasks"></i>
-				<p>Plan your income and payments in advance with Paycheckit.</p>
-				<p>Got paid? <i className="far fa-money-bill-alt"></i></p>
-				<p><i className="fas fa-credit-card"></i> Paid a bill?</p>
+				<h2>Plan your income and payments in advance with Paycheckit.</h2>
+					<div className="font-awesome">			
+						<i className="fas fa-4x fa-tasks"></i>
+					</div>
+				<p>Got paid? <i className="far fa-2x fa-money-bill-alt"></i></p>
+				<p><i className="fas fa-2x fa-credit-card"></i> Paid a bill?</p>
 				<p>Check it off!</p>
-				<i className="far fa-check-square"></i>
+				<i className="far fa-3x fa-check-square"></i>
+				<div className="font-awesome">			
+						<i className="fas fa-3x fa-angle-double-down" data-fa-transform="down-20"></i>
+					</div>
 			</section>
 			<section>
 				{/** LOGIN **/}
