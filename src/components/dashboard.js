5dda82dda72ce95b806eff19bba7fe0287c7d009
Nicole Bradford <nbradf2@gmail.com>
2018-01-31 19:44:47 -0600

    add landing page component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import '../normalize.css';
+import '../grid.css';
 
 export default function Dashboard(props) {
-	return (<h1>Hello World</h1>);
+	return (
+		<div>
+			{/** NAV BAR **/}
+			<section>
+				<h1>PaycheckIt</h1>
+				<p>Budget your money</p>
+				<p>Because living paycheck to paycheck doesn't have to be so hard.</p>
+			</section>
+			<section>
+				<i class="fas fa-tasks"></i>
+				<p>Plan your income and payments in advance with Paycheckit.</p>
+				<p>Got paid? <i class="far fa-money-bill-alt"></i></p>
+				<p><i class="fas fa-credit-card"></i> Paid a bill?</p>
+				<p>Check it off!</p>
+				<i class="far fa-check-square"></i>
+			</section>
+			<section>
+				{/** LOGIN **/}
+			</section>
+		</div>
+	);
 }
