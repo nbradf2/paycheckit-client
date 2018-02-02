50bda5b3c3c69de741f0b805f09a2685b3240ab7
Nicole Bradford <nbradf2@gmail.com>
2018-02-01 20:25:50 -0600

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LandingPage from './components/landingPage';
+import './normalize.css';
+import './grid.css';
 import './index.css';
-
 
 ReactDOM.render(<LandingPage />, document.getElementById('root'));
 
