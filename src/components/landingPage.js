34ff937804d9ac0a65c8a8f4ce7fa70f4f239bdc
Nicole Bradford <nbradf2@gmail.com>
2018-02-03 08:38:16 -0600

    fix centering of login inputs

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Footer from './footer'
-import LoginForm from './loginForm'
+import LoginForm from './loginForm';
+import '../normalize.css';
+import '../index.css';
 import './landingPage.css';
 
 export default function LandingPage(props) {
@@ -12,13 +14,13 @@ export default function LandingPage(props) {
 					<h1>Paycheck<span>It</span></h1>
 					<p>Because living paycheck to paycheck doesn't have to be so hard.</p>
 				</div>
-				<i className="fas fa-3x fa-angle-double-down" data-fa-transform="down-60"></i>
+					<i className="fas fa-3x fa-angle-double-down" data-fa-transform="down-60"></i>
 			</section>
 			<section>
 				<h2>Plan your income and payments in advance with Paycheckit.</h2>		
 				<i className="fas fa-5x fa-tasks"></i>
-				<p class="left-side">Got paid? <i className="far fa-2x fa-money-bill-alt"></i></p>
-				<p class="right-side"><i className="fas fa-2x fa-credit-card"></i> Paid a bill?</p>
+				<p className="left-side">Got paid? <i className="far fa-2x fa-money-bill-alt"></i></p>
+				<p className="right-side"><i className="fas fa-2x fa-credit-card"></i> Paid a bill?</p>
 				<p>Check it off!</p>
 				<i className="far fa-3x fa-check-square"></i>
 				<div className="font-awesome">			
