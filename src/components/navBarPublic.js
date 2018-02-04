134acd3373e24e3f4ecb398cdeed8b13e47f9d1a
Nicole Bradford <nbradf2@gmail.com>
2018-02-04 11:25:14 -0600

    add styling

--- src/components/navBarPublic.js
+++ src/components/navBarPublic.js
@@ -3,16 +3,16 @@ import './navBarPublic.css';
 
 export default function NavBarPublic(props) {
 	const links = props.links.map((link, index) =>(
-		<li key={index}>
+		<li key={index} className="nav-links">
 			<a href={link.href}>
 				{link.text}
 			</a>
 		</li>
 	));
 	return (
-		<div>
-			<h1>{props.title}</h1>
-			<nav>
+		<div className="nav-bar-public row">
+			<h1 className="col-6" id="title">{props.title}</h1>
+			<nav className="nav-public col-6">
 				<ul>
 					{links}
 				</ul>
