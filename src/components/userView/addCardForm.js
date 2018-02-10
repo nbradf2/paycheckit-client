7db29105c187a8b037a632bc559a5ea48ff28bd7
Nicole Bradford <nbradf2@gmail.com>
2018-02-10 09:41:18 -0600

    add styling to addcard

--- src/components/userView/addCardForm.js
+++ src/components/userView/addCardForm.js
@@ -3,9 +3,9 @@ import './addCardForm.css';
 
 export default function AddCardForm(props) {
 	return(
-		<form id="addCard">
-			<h2>Add Item</h2>
-			<div className="input">
+		<form id="addCard" className="row">
+			<div className="input col-9">
+				<h2>Add Item</h2>
 				<label htmlFor="itemDate" hidden>itemDate</label>
 				<input type="text" name="itemDate" id="itemDate" placeholder="Date"/>
 				<label htmlFor="itemAmount" hidden>Item Amount</label>
@@ -13,14 +13,14 @@ export default function AddCardForm(props) {
 				<label htmlFor="itemLabel" hidden>Item Label</label>
 				<input type="text" name="itemLabel" id="itemLabel" placeholder="Label"/>
 				<div className="paymentType">
-					<p className="income">Income</p>
+					<h4 className="income">Income</h4>
 					<input type="checkbox" name="paycheck" id="paycheck" />
 					<label htmlFor="paycheck">Paycheck</label>
 					<input type="checkbox" name="addSavings" id="addSavings" />
 					<label htmlFor="addSavings">Savings</label>
 					<input type="checkbox" name="addOther" id="addOther" />
 					<label htmlFor="addOther">Other</label>
-					<p className="payment">Payment</p>
+					<h4 className="payment">Payment</h4>
 					<input type="checkbox" name="credtCard" id="creditCard"/>
 					<label htmlFor="creditCard">Credit Card</label>
 					<input type="checkbox" name="minusSavings" id="minusSavings"/>
@@ -34,8 +34,8 @@ export default function AddCardForm(props) {
 					<input type="checkbox" name="minusOther" id="minusOther"/>
 					<label htmlFor="minusOther">Other</label>
 				</div>
+				<button type="submit">Add Item</button>
 			</div>
-			<button type="submit">Add Item</button>
 		</form>
 	);
 }
