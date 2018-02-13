6f351e6e9e0f85b04c5686cafd039e430c1fa2b6
Nicole Bradford <nbradf2@gmail.com>
2018-02-12 20:25:21 -0600

    add reducer to include ADD_BUDGETCARD and ADD_BUDGETLIST

--- src/reducers/budgetReducer.js
+++ src/reducers/budgetReducer.js
@@ -1,42 +1,99 @@
 import * as actions from '../actions/budgetActions';
 
 const initialState = {
-	month: 'January 2018',
-	income: 2000,
-	startingBalance: 1000,
-	endingBalance: 3000,
-	lineItems: [
-		{
-			checked: false,
-			date: '02/02/18',
-			amount: 1000,
-			label: 'Paycheck'
-		},
-		{
-			checked: false,
-			date: '02/10/18',
-			amount: 400,
-			label: 'Savings'
-		},
-		{
-			checked: false,
-			date: '02/10/18',
-			amount: -75,
-			label: 'MasterCard'
-		},
-		{
-			checked: false,
-			date: '02/02/18',
-			amount: -105,
-			label: 'Verizon'
-		}
-	]
-
+	budgetList: [{
+		month: 'January 2018',
+		income: 2000,
+		startingBalance: 1000,
+		endingBalance: 3000,
+		lineItems: [
+			{
+				checked: false,
+				date: '02/02/18',
+				amount: 1000,
+				label: 'Paycheck'
+			},
+			{
+				checked: false,
+				date: '02/10/18',
+				amount: 400,
+				label: 'Savings'
+			},
+			{
+				checked: false,
+				date: '02/10/18',
+				amount: -75,
+				label: 'MasterCard'
+			},
+			{
+				checked: false,
+				date: '02/02/18',
+				amount: -105,
+				label: 'Verizon'
+			}
+		]
+	}, {
+		month: 'February 2018',
+		income: 3000,
+		startingBalance: 900,
+		endingBalance: 3000,
+		lineItems: [
+			{
+				checked: false,
+				date: '02/02/18',
+				amount: 1000,
+				label: 'Paycheck'
+			},
+			{
+				checked: false,
+				date: '02/10/18',
+				amount: 400,
+				label: 'Savings'
+			},
+			{
+				checked: false,
+				date: '02/10/18',
+				amount: -75,
+				label: 'MasterCard'
+			},
+			{
+				checked: false,
+				date: '02/02/18',
+				amount: -105,
+				label: 'Verizon'
+			}
+		]
+	}]
 };
 
 export default function budgetReducer(state=initialState, action) {
-	
-	{/*Series of IF statements that will return new versions of the state*/}
+	if (action.type === actions.ADD_BUDGETLIST) {
+		return Object.assign({}, state, {
+			budgetList: [...state.lists, {
+				month: action.month,
+				lineItems: []
+			}]
+		});
+	}
+	else if (action.type === actions.ADD_BUDGETCARD) {
+		let budgetList = state.budgetList.map((budgetList, index) => {
+			if (index !== action.budgetListIndex) {
+				return budgetList;
+			}
+			return Object.assign({}, budgetList, {
+				lineItems: [...budgetList.lineItems, {
+					checked: action.checked,
+					date: action.date,
+					amount: action.amount,
+					label: action.label
+				}]
+			});
+		});
+
+		return Object.assign({}, state, {
+			budgetList
+		});
+	}
 	return state;
 }
 
