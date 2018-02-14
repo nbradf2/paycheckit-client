819d268fce4375879eaf932cea117af8e302fefc
Nicole Bradford <nbradf2@gmail.com>
2018-02-13 19:58:21 -0600

    update card.js to render correct props

--- src/components/userView/budgetCard.js
+++ src/components/userView/budgetCard.js
@@ -2,43 +2,21 @@ import React from 'react';
 import {connect} from 'react-redux'
 import './budgetCard.css';
 
-function BudgetCard(props) {
-	
-	let rows = props.budget.map(function(line, index) {
-		return (
-			<div className="line" key={index}>
-				<input type="checkbox" id="checkbox"/>
-				<label htmlFor="checkbox" hidden></label>
-				<p className="item">{line.date}</p>
-				<p className="item">{line.amount}</p>
-				<p className="item">{line.label}</p>
-				<p className="item"><i className="fa fa-pencil"></i></p>
-				<p className="item"><i className="fa fa-trash-o"></i></p>
-			</div>
-		)
-	});
-
-	return(
-		// will pull from the current list - mapStateToProps(array of budgets)
-		// will iterate over an array of BudgetCards
-		// get data group by month through mongo
-		<div className="budgetTable col-4">
-			{/*starting balance*/}
-			{/*income balance*/}
-			{/*ending balance*/}
-			<h3>{props.date}</h3>
-			<h3>{props.startingBalance}</h3>
-			<div>{rows}</div>
-			<button id="addNewBudget">Add New Budget</button>
-			{/*Show detail button on top right of card*/}
+export default function BudgetCard(props) {
+	return (
+		<div>
+			{props.checked}
+			{props.date}
+			{props.amount}
+			{props.label}
 		</div>
-	);
+	)
+	
 }
 
-const mapStateToProps = state => ({
-	date: state.date,
-	startingBalance: state.startingBalance,
-	budget: state.budget
-})
-
-export default connect(mapStateToProps)(BudgetCard)
+BudgetCard.defaultProps = {
+	checked: '',
+	date: '',
+	amount: '',
+	label: ''
+}
