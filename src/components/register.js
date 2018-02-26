5a3cfc97e54ad3fd34eb865036d67bf453b02781
Nicole Bradford <nbradf2@gmail.com>
2018-02-25 19:47:36 -0600

    add input.js

--- src/components/register.js
+++ src/components/register.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Field, reduxForm, focus} from 'redux-form';
 import {registerUser} from '../actions/users'
 import {login} from '../actions/auth';
-
+import Input from './input';
 
 import {Link} from 'react-router-dom';
 import './register.css';
