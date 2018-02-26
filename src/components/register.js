ec07afd354bf716c4fdc468f711426565dc50099
Nicole Bradford <nbradf2@gmail.com>
2018-02-25 19:54:14 -0600

    add validators

--- src/components/register.js
+++ src/components/register.js
@@ -3,6 +3,7 @@ import {Field, reduxForm, focus} from 'redux-form';
 import {registerUser} from '../actions/users'
 import {login} from '../actions/auth';
 import Input from './input';
+import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
 
 import {Link} from 'react-router-dom';
 import './register.css';
